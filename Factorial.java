37367ab1f7aa293eb0f3a77058179fec21080b34
Illia <ryabchenko.mail@gmail.com>
2018-10-07 17:40:28 +0300

    first commit

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Print the value of positive integer, which is n!");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        int factorialNumber = 1;
+        int factorial = 1;
+        while (factorial<inputNumber){
+            factorialNumber++;
+            factorial *=factorialNumber;
+        }
+        System.out.println("The value of n " + factorialNumber);
+    }
+}
---
+++ MatchesCount.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.Scanner;
+
+public class MatchesCount {
+    public static void main(String[] args) {
+        System.out.println("Input number of squares from 1 to 10^9");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfSquares = scanner.nextInt();
+        double columnNumber =  Math.floor(Math.sqrt(numberOfSquares));
+        double rowNumber = Math.ceil(numberOfSquares/columnNumber);
+        double extraSquares = numberOfSquares - columnNumber*rowNumber;
+        double matchesNumber = numberOfSquares*4 - ((columnNumber-1)*rowNumber +(rowNumber-1)*columnNumber)
+                - extraSquares*2;
+
+        System.out.println("The minimum number of matches required to construct " +
+                numberOfSquares + " squares = " + matchesNumber);
+        scanner.close();
+
+    }
+}
